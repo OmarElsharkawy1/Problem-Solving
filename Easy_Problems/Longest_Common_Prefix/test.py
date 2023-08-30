cc431e3541595fbf6629b66334aee875f7d919cb
Omar Elsharkawy <omarelsharkawy7@gmail.com>
2023-08-31 01:56:26 +0300

    Add test case

---
+++ Easy_Problems/Longest_Common_Prefix/test.py
@@ -0,0 +1,13 @@
+import unittest
+import longest_common_prefix
+
+class TestLongestCommonPrefix(unittest.TestCase):
+
+    def test_case1(self):
+        self.assertEqual(longest_common_prefix.longestCommonPrefix(['cat', 'car', 'carpenter']), 'ca')
+        self.assertEqual(longest_common_prefix.longestCommonPrefix(['cartoon', 'cartoons', 'carpenter']), 'car')
+        self.assertEqual(longest_common_prefix.longestCommonPrefix(['dog', 'animal']), '')
+    
+
+if __name__ == '__main__':
+    unittest.main()
