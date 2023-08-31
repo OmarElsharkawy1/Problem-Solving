1027b627ac07e4ce8f981478dd7aa9cf4d8ce216
Omar Elsharkawy <omarelsharkawy7@gmail.com>
2023-08-31 18:25:30 +0300

    Add comment , documentation to valid_parentheses

--- Easy_Problems/Valid_Parentheses/valid_parentheses.py
+++ Easy_Problems/Valid_Parentheses/valid_parentheses.py
@@ -1,15 +1,18 @@
-
 def isValid(s: str):
-    possible_str = {'(': ')', '{': '}', '[' : ']'}
-    openings = []
+    possible_str = {'(': ')', '{': '}', '[' : ']'}  # Define a dictionary of possible opening and closing symbols
+    openings = []  # Initialize an empty list to store opening symbols
 
     if len(s) % 2 != 0:
-        return False
-    
+        return False  # If the length of the input string is odd, it cannot be valid
+
     for symbol in s:
+        # Iterate through each character in the input string
         if symbol in possible_str.keys():
+            # If the symbol is an opening symbol, add it to the 'openings' list
             openings.append(symbol)
         elif openings == [] or symbol != possible_str[openings.pop()]:
-            return False
-    
-    return openings == []
+            # If the symbol is a closing symbol, check if it matches the corresponding opening symbol
+            # If the 'openings' list is empty or the current symbol does not match the expected closing symbol
+            return False  # Return False, indicating an invalid string
+
+    return openings == []  # Return True if all opening symbols have been closed, otherwise return False
